8b9ff6e0ed602f9fa9e8a2a05ac315297516f45a
Liza101111 <lixia101111@gmail.com>
2025-12-17 11:26:04 +0200

    optimized db path

--- db/init_db.py
+++ db/init_db.py
@@ -1,9 +1,14 @@
+from pathlib import Path
 import sqlite3
 
-conn = sqlite3.connect("../support_bot.db")
+BASE_DIR = Path(__file__).resolve().parent
+DB_PATH = BASE_DIR.parent / "support_bot.db"
+SCHEMA_PATH = BASE_DIR / "schema.sql"
+
+conn = sqlite3.connect(DB_PATH)
 cursor = conn.cursor()
 
-with open("schema.sql", "r", encoding="utf-8") as f:
+with open(SCHEMA_PATH, "r", encoding="utf-8") as f:
     schema = f.read()
 
 cursor.executescript(schema)
